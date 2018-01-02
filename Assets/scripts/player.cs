81b0e46402e76b32b45040fdf5f9c68e1c53c68f
inbal Bruker <inbali.bruker@gmail.com>
2018-01-02 19:22:32 +0200

    player

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour {
+	[Header("Movement")]
+	[SerializeField] float xSpeed;
+	[SerializeField] float ySpeed;
+	[Range(0, 1)]
+	[SerializeField] float t;
+	private Rigidbody2D body;
 
-	// Use this for initialization
-	void Start () {
-		
+
+	void Awake ()
+	{
+		body = GetComponent<Rigidbody2D>();
+
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update ()
+	{
+
+		var velocity = body.velocity;
+		if (Input.GetKey ("up")) {
+			velocity.y = ySpeed;
+		}
+		if (Input.GetKey ("down")) {
+			velocity.y = -ySpeed;
+		}
+		if (Input.GetKey ("left")) {
+			velocity.x = -xSpeed;
+		}
+		if (Input.GetKey ("right")) {
+			velocity.x = xSpeed;
+		}
+		body.velocity = Vector2.Lerp(body.velocity, velocity, t);
 	}
+
 }
