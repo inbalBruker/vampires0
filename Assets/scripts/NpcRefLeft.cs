ac0c28ed6eedfe44cf3cc6abe0177ce1f19e02f1
inbal Bruker <inbali.bruker@gmail.com>
2018-01-08 16:46:06 +0200

    mirror

--- Assets/scripts/NpcRefLeft.cs
+++ Assets/scripts/NpcRefLeft.cs
@@ -8,15 +8,27 @@ public class NpcRefLeft : MonoBehaviour {
 	// Use this for initialization
 	void Awake ()
 	{
-//		
 		dis = 14;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		float y = npc.transform.position.y;
+//		float sort = Mathf.InverseLerp (-14, 0, y);
+//		print ("sort: " + sort);
+//		dis = Mathf.FloorToInt(Mathf.Lerp (14, 1, sort));
+//		if (y < 0) {
+//			dis = 14;
+//		}
+//		print ("dis:" + dis);
 		float x = npc.transform.position.y;
+
+		var deg = 60;
+		var rad = deg * Mathf.PI / 180;
+
+		dis = 14 - (y / Mathf.Tan (rad)) - x;
 		float thisX = -dis - (dis - x);
 		if (thisX < -dis) {
 			thisX = -dis;
--- Assets/scripts/playerTest.cs
+++ Assets/scripts/playerTest.cs
@@ -16,6 +16,8 @@ public class playerTest : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float y = npc.transform.position.y;
+		float sort = Mathf.InverseLerp (-14, 0, y);
+		dis = Mathf.FloorToInt(Mathf.Lerp (14, 0, sort));
 		float x = npc.transform.position.y;
 		float thisX = -dis - (dis - x);
 		this.transform.position = new Vector3(thisX, y, transform.position.z);
